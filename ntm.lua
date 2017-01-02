a4c9bde14e34dd859acf8b44361e0a0830f1f437
Tom Veniat <veniat.tom@gmail.com>
2017-01-02 23:32:08 +0100

    Change dataset generation + running test

--- ntm.lua
+++ ntm.lua
@@ -8,11 +8,18 @@ function createSample(sampleSize)
 end
 
 function createCopyDataSet(nSample, sampleSize)
-	local dataSet = torch.Tensor(nSample, sampleSize)
+	local x = torch.zeros(nSample+2, sampleSize+2)
+	local y = torch.zeros(nSample, sampleSize)
 	for i=1,nSample do
-		dataSet[{{i},{}}] = createSample(sampleSize) 
+		local sample = createSample(sampleSize)
+		x[{{i+1},{}}] = torch.cat(sample, torch.Tensor({0,0}))
+		y[{{i},{}}] = sample
 	end
-	return dataSet, dataSet
+
+	x[{{1},{sampleSize + 1}}] = 1
+	x[{{nSample+2},{sampleSize + 2}}] = 1
+
+	return x, y
 end
 
 
@@ -21,13 +28,16 @@ end
 m = Memory(5,2)
 print(m.mem)
 
--- r = torch.Tensor({0.9,0.1,0,0,0})
--- print(r:resize(1,5))
--- print(m:read(r))
+r = torch.Tensor({0.9,0.1,0,0,0})
+print(r:resize(1,5))
+print(m:read(r))
 
 
-r = torch.Tensor({-2,1}):resize(1,2)
-print(m:getContentWeightings(r))
+r = torch.Tensor({1,2}):resize(1,2)
+print(m:getContentWeightings(r,120))
 
 
 xs,ys = createCopyDataSet(10,4)
+
+print(xs)
+print(ys)
