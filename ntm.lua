65b5e7c92eca05edb9617b5fcf797ded1e3cce1b
Tom Veniat <veniat.tom@gmail.com>
2017-01-07 03:52:40 +0100

    Implem NTM head weightings part I

--- ntm.lua
+++ ntm.lua
@@ -1,4 +1,5 @@
 require 'nn'
+require 'nngraph'
 require 'Memory'
 
 function createSample(sampleSize)
@@ -41,6 +42,7 @@ xs,ys = createCopyDataSet(10,4)
 
 print(xs)
 print(ys)
+print(m.mem)
 
 print()
 
@@ -69,3 +71,52 @@ m:erase(r:t(),e)
 print(m.mem)
 m:add(r:t(),e)
 print(m.mem)
+
+a = torch.Tensor({0,1,1,0,1})
+b = torch.Tensor({0,0,1,0,1})
+c = torch.Tensor({0,1,1,0,0})
+d = torch.Tensor({-700,12,123,-14,6})
+e = torch.Tensor({-700,-45,123,-14,6})
+
+
+criterion = nn.BCECriterion()
+
+local NTM, Parent = torch.class('nn.NTM', 'nn.Module')
+
+function NTM:__init(nInput, nOutput)
+   Parent.__init(self)
+
+   self.mem_locations = 10
+   self.mem_location_size = 5
+   self.hidden_state_size = 100
+   self.allowed_shifts = {-1,0,1}
+
+   self.mem = torch.range(1, mem_locations * mem_location_size):resize(mem_locations, mem_location_size)
+
+   self:init_controller()
+
+end
+
+function NTM:init_controller(input)
+	h = self:create_head()
+
+end
+
+function NTM:create_head(h_state, prev_w, mem)
+	k_t = nn.tanh()(nn.Linear(self.hidden_state_size, self.mem_locations)(h_state))
+
+	beta_t = nn.SoftPlus()(nn.Linear(self.hidden_state_size, 1)(h_state))
+
+	g_t = nn.Sigmoid()(nn.Linear(self.hidden_state_size, 1)(h_state))
+
+	s_t = nn.SoftMax()(nn.Linear(self.hidden_state_size)(#self.allowed_shifts)(h_state))
+
+	gamma_t = nn.AddConstant(1)(nn.SoftPlus()(nn.Linear(self.hidden_state_size, 1)(h_state)))
+
+
+
+end
+
+function NTM:forward(input)
+
+end
