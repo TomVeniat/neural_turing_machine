0028a845c1f9141a26d0141d3b997965cdf64b68
Tom Veniat <veniat.tom@gmail.com>
2017-01-08 04:19:24 +0100

    Create logging module.

---
+++ modules/Logging.lua
@@ -0,0 +1,26 @@
+local Logging, _ = torch.class('nn.Logging', 'nn.Module')
+
+
+function Logging:__init(name, active)
+	self.name = name
+	if active == nil then
+		active = true
+	end
+	self.active = active
+end
+
+
+function Logging:updateOutput(input)
+	self.output = input
+	if self.active then 
+		print(self.name .. ' :')
+		print(input)
+	end
+	return self.output
+end
+
+
+function Logging:updateGradInput(input, gradOutput)
+	self.gradInput = gradOutput
+	return self.gradInput
+end
